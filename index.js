6cbe6f59a21cddd6cc54e9e8d1166c8d7e2a4bb5
Amine Mouafik <amine@mouafik.fr>
2014-09-15 10:31:08 +0700

    Stealth mode.

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 var _ = require('lodash');
 var soap = require('soap');
 
-module.exports = TriangleCRM;
+module.exports = MRCelgnairT;
 
-TriangleCRM.ENDPOINT = '/api/2.0/billing_ws.asmx?wsdl';
+MRCelgnairT.ENDPOINT = '/api/2.0/billing_ws.asmx?wsdl';
 
-function TriangleCRM(options, callback) {
+function MRCelgnairT(options, callback) {
   var _this = this;
   _this.client = null;
-  _this.endpoint = TriangleCRM.ENDPOINT;
+  _this.endpoint = MRCelgnairT.ENDPOINT;
 }
 
-function TriangleCRM(options, callback) {
+function MRCelgnairT(options, callback) {
   this._host = options.host;
   this._username = options.username;
   this._password = options.password;
@@ -26,22 +26,22 @@ function TriangleCRM(options, callback) {
   };
 }
 
-TriangleCRM.prototype.initialize = function(callback) {
+MRCelgnairT.prototype.initialize = function(callback) {
   var _this = this;
-  soap.createClient(_this._host + TriangleCRM.ENDPOINT, function(error, client) {
+  soap.createClient(_this._host + MRCelgnairT.ENDPOINT, function(error, client) {
     _this.client = (!error) ? client : error;
     callback && callback(error, client);
   });
 };
 
-TriangleCRM.prototype.signRequest = function(request) {
+MRCelgnairT.prototype.signRequest = function(request) {
   return _.extend(request, {
     username: this._username,
     password: this._password
   });
 };
 
-TriangleCRM.prototype.processRequest = function(service, request, callback) {
+MRCelgnairT.prototype.processRequest = function(service, request, callback) {
   this.client[service](this.signRequest(request), function(error, result) {
     if (error)
       return callback && callback(error);
@@ -52,24 +52,24 @@ TriangleCRM.prototype.processRequest = function(service, request, callback) {
   });
 };
 
-TriangleCRM.prototype.CreateProspect = function(prospect, callback) {
+MRCelgnairT.prototype.CreateProspect = function(prospect, callback) {
   prospect.productTypeIDSpecified = (prospect.productTypeId) ? 'TRUE' : 'FALSE';
   this.processRequest('CreateProspect', prospect, callback);
 };
 
-TriangleCRM.prototype.CreateSubscription = function(subscription, callback) {
+MRCelgnairT.prototype.CreateSubscription = function(subscription, callback) {
   subscription.campaignIDSpecified = (subscription.campaignID) ? 'TRUE' : 'FALSE';
   subscription.prospectIDSpecified = (subscription.prospectID) ? 'TRUE' : 'FALSE';
   subscription.paymentTypeSpecified = (subscription.paymentType) ? 'TRUE' : 'FALSE';
   this.processRequest('CreateSubscription', subscription, callback);
 };
 
-TriangleCRM.prototype.CreateNewSubscriptionExistingCustomer = function(upsell, callback) {
+MRCelgnairT.prototype.CreateNewSubscriptionExistingCustomer = function(upsell, callback) {
   upsell.prospectIDSpecified = (upsell.prospectID) ? 'TRUE' : 'FALSE';
   upsell.billingIDSpecified = (upsell.billingID) ? 'TRUE' : 'FALSE';
   this.processRequest('CreateNewSubscriptionExistingCustomer', upsell, callback);
 };
 
-TriangleCRM.prototype.GetSaleInfo = function(sale, callback) {
+MRCelgnairT.prototype.GetSaleInfo = function(sale, callback) {
   this.processRequest('GetSaleInfo', sale, callback);
 };
